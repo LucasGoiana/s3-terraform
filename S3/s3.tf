bd67805c39f33708a4151cb92753ccdb160fc8af
LucasGoianaMalicia <lucasgoianam@hotmail.com>
2022-03-18 17:09:36 -0300

    commit

--- S3/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "s3" {
-  bucket = "trabalho-final-grupo1-teste-modulo"
-
-  tags = {
-    Name        = "trabalho-final-grupo1-teste-modulo"
-    Environment = "admin"
-  }
-}
--- STATE/state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "trabalho-final-grupo1-teste-modulo"
-    key    = "teste"
-    region = "us-east-1"
-  }
-}
